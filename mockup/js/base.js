443f412c7dbf22d7f18aac40ba0dbd47dd1759bd
wataru.otsuji <wataru.otsuji@gmail.com>
2023-09-18 09:09:20 +0900

    gitignore update

--- mockup/js/base.js
+++ mockup/js/base.js
@@ -6,3 +6,9 @@ document.querySelector('#collapseWidthExample').addEventListener('hide.bs.collap
 document.querySelector('#collapseWidthExample').addEventListener('show.bs.collapse', function () {
     document.querySelector('.side_menu_text').innerHTML = "&lt;&lt;";
 });
+window.onload = function () {
+    var wd = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    if (wd <= 575) {
+        document.getElementById("side_menu_button").click();
+    }
+}
